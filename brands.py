31ac94e23d486f365643c6948d1b956ae84c45a5
jazmin-H <huaynocajazmin999@email.com>
2024-10-07 09:55:13 -0300

    agregue el archivo brands que me permite visualizar los tipos de marca de los productos y aparte me permite mostrar un dato en especifico utilizando los ID

---
+++ brands.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify
+import mariadb
+
+app = Flask(__name__)
+
+@app.route("/brands")
+def brands():
+    mari = mariadb.connect(
+        user = "uniondepo",
+        password ="uniondepo111",
+        host ="10.9.120.5",
+        database= "uniondepo"
+    )
+    cur = mari.cursor()
+    cur.execute("SELECT * FROM brands")
+
+    color = [column[0] for column in cur.description]
+    
+    tabla = []
+    for row in cur:
+        tabla.append(dict(zip(color, row)))
+
+    return jsonify(tabla)
+
+
+@app.route("/brands/<int:id>")
+def detalle_brands(id):
+    mari = mariadb.connect(
+        user = "uniondepo",
+        password ="uniondepo111",
+        host ="10.9.120.5",
+        database= "uniondepo"
+    )
+    cur = mari.cursor()
+    cur.execute("SELECT * FROM brands WHERE ID= ?", (id,))
+
+    color = [column[0] for column in cur.description]
+    
+    tabla = [dict(zip(color, row)) for row in cur.fetchall()]
+
+    return jsonify(tabla)
