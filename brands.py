432aebeee85bb7e82c7e2cf5a27be5a03ff2b990
Jonatan <togo36918@gmail.com>
2024-11-11 10:38:54 -0300

    Se agrego el flask_cords que no tenia

--- brands.py
+++ brands.py
@@ -1,7 +1,11 @@
-from flask import Flask, jsonify
+
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 import mariadb
+from mariadb import Error
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/brands")
 def brands():
